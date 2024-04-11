8c6cd8991187e9452713f33008ab74ffc3c80d0e
georgetate <157422491+georgetate@users.noreply.github.com>
2024-04-11 13:35:18 -0600

    Update helloworld.cs

--- helloworld.cs
+++ helloworld.cs
@@ -1,1 +1,10 @@
+using System;
 
+namespace HelloWorldinC# {
+  class Geeks {
+    static voic Main(string[] args) {
+      Console.WriteLine("Hello World from C#");
+      Console.ReadKey();
+    }
+  }
+}
